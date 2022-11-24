fe1741303439fb5e5894b46a50f02ff31ae00e92
jnzbfgjd3180 <m15927337875@163.com>
2022-11-24 01:04:39 -0500

    small fix

--- client/workersetup.sh
+++ client/workersetup.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install python3-pip
-
-sudo pip3 install hashlib
-sudo pip3 install socket
-sudo pip3 install json
-sudo pip3 install multiprocessing
-sudo pip3 install math
-
 sudo python3 client.py $1
--- server/serversetup.sh
+++ server/serversetup.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install python3-pip
-
-sudo pip3 install socket
-sudo pip3 install json
-sudo pip3 install multiprocessing
-sudo pip3 install flask
-
 sudo python3 server.py
